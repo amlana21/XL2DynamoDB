968edbc97504910fde6f04c661a240d2b70d84c9
amlan <amlan@DESKTOP-IRNASE0>
2018-01-05 23:15:56 -0800

    ui modified

--- src/xl2dynamodb/appuiController.java
+++ src/xl2dynamodb/appuiController.java
@@ -10,6 +10,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -22,9 +24,27 @@ public class appuiController implements Initializable {
     private Label label;
     
     @FXML
+    private Button browsebtn;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+    }
+    
+    @FXML
+    private void browsefilebtn(ActionEvent event) {
+        //infoBox1("Please use brackets for multiple conditions in WHERE", "Error",null);
+        
+    }
+    
+    public static void infoBox1(String infoMessage, String titleBar, String headerMessage)
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titleBar);
+        alert.setHeaderText(headerMessage);
+        alert.setContentText(infoMessage);
+        alert.showAndWait();
     }
     
     @Override
