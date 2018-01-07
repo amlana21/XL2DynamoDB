801d0c371add348eaae77dd78657b79c11db4d2b
amlan <amlan@DESKTOP-IRNASE0>
2018-01-07 13:16:07 -0800

    login ui code

--- src/xl2dynamodb/LoginuiController.java
+++ src/xl2dynamodb/LoginuiController.java
@@ -7,10 +7,17 @@ package xl2dynamodb;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  * FXML Controller class
@@ -31,9 +38,32 @@ public class LoginuiController implements Initializable {
     @FXML
     private Button loginawsbtn;
     
+    @FXML
+    private void loginbtnaction(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("appui.fxml"));
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        //((event.getSource())).getScene().getWindow().hide();
+        getWindow(loginawsbtn).hide();
+        
+    }
     
-    
-    
+    private Window getWindow(Node node)
+{
+	Window window = null;
+
+	Scene scene = node.getScene();
+
+	if (scene != null)
+	{
+		window = scene.getWindow();
+	}
+
+	return window;
+}
     
     /**
      * Initializes the controller class.
