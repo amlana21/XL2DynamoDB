33c2ddb52c594ada4aa06437174982260e78c0b4
amlan <>
2018-01-10 11:37:05 -0800

    WIP-insert item

--- src/xl2dynamodb/LoginuiController.java
+++ src/xl2dynamodb/LoginuiController.java
@@ -19,6 +19,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,6 +29,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -44,10 +47,19 @@ public class LoginuiController implements Initializable {
     private TextField accskyidtxt;
      @FXML
     private TextField scaccskytxt;
-     @FXML
+     @FXML  
     private TextField regiontxt;
-     
-     
+     @FXML
+    private ChoiceBox regionchoice;
+     //ObservableList<String> availableChoices = FXCollections.observableArrayList("apples", "oranges"); 
+     //regionchoice.setItems(availableChoices);
+             
+             
+             
+     //regionchoice.
+        //regionchoice.setItems(availableChoices);
+
+
     @FXML
     private Button loginawsbtn;
     
