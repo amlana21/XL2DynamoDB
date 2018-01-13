2549285233eff0bcfef5a4206ec066ba05723bc7
amlan <amlan@DESKTOP-IRNASE0>
2018-01-13 00:08:37 -0800

    Final Code

--- src/xl2dynamodb/XL2Dynamodb.java
+++ src/xl2dynamodb/XL2Dynamodb.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *@fileoverview Main class
+ * @author amlan
  */
 package xl2dynamodb;
 
@@ -19,13 +18,7 @@ public class XL2Dynamodb extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        /*
-        Parent root = FXMLLoader.load(getClass().getResource("appui.fxml"));
         
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();*/
         Parent root = FXMLLoader.load(getClass().getResource("loginui.fxml"));
         stage.resizableProperty().setValue(Boolean.FALSE);
         Scene scene = new Scene(root);
