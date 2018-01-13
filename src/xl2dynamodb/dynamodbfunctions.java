6c574ad760879f8e98708de96513cad822c74fc8
amlan <>
2018-01-12 16:57:32 -0800

    Final Code

--- src/xl2dynamodb/dynamodbfunctions.java
+++ src/xl2dynamodb/dynamodbfunctions.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *@fileoverview Controller file for the select file UI
+ * @author amlan
  */
+
 package xl2dynamodb;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -21,48 +21,11 @@ import java.util.List;
 public class dynamodbfunctions {
     public void insertItemtodb(String acckeystr,String seckeystr,String rgnstr,List inparry,String tablnme) throws Exception{
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(acckeystr,seckeystr);
-        /*
-        //--------------------aws actions working---------------
-        try{
-        //------------------test aws call-----------
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJSLUEWDCVJD6BTLQ", "rH4D7vHzRNG3BHYXXazcMjB8qyqk51ClcgBosYEx");
-       AmazonDynamoDB dynamoDB;
-        dynamoDB = AmazonDynamoDBClientBuilder.standard()
-            .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-            .withRegion("us-east-1")
-            .build();
-// Describe our new table
-            DescribeTableRequest describeTableRequest = new DescribeTableRequest().withTableName("codeaccess");
-            TableDescription tableDescription = dynamoDB.describeTable(describeTableRequest).getTable();
-            System.out.println("Table Description: " + tableDescription);
-        //------------------test aws call-----------
-        
-        //----------------------test put item-----------------
-        DynamoDB dynamoDBtble = new DynamoDB(dynamoDB);
-        Table table = dynamoDBtble.getTable("codeaccess");
-        Item item = new Item().withPrimaryKey("id", 120);
-        item.withString("name", "Book 120 Title99");
-                item.withString("status", "120-99");
-                item.withString("type", "type_991");
-            table.putItem(item);
-         //----------------------test put item-----------------
-        }
-        catch (Exception ex1) {
-            
-            System.out.println(ex1.getMessage());
-        }
-        
-        //--------------------aws actions working---------------
-        
-        
-        */
         
         //------------------get column names-------
         int arrcnt=0;
-        //Object[] arr1=inparry[arrcnt];
         Object[] arr1=(Object[])inparry.get(arrcnt);
         arrcnt=arrcnt+1;
-        
         //------------------get column names-------
         
         //-----------------get column types-----
@@ -76,8 +39,7 @@ public class dynamodbfunctions {
         AmazonDynamoDB dynamoDB;
         dynamoDB = AmazonDynamoDBClientBuilder.standard()
             .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-            //.withRegion("us-east-1")
-              .withRegion(rgnstr)
+            .withRegion(rgnstr)
             .build();
         DynamoDB dynamoDBtble = new DynamoDB(dynamoDB);
         Table table = dynamoDBtble.getTable(tablnme);
@@ -90,7 +52,6 @@ public class dynamodbfunctions {
           colcnt=colcnt+1;
           while(colcnt<rw.length){
               String cnme=arr1[colcnt].toString();
-              //String cvlue= rw[colcnt].toString();
               if(dtypes[colcnt].toString().equals("S")){
                   String cvlue= rw[colcnt].toString();
                   item.withString(cnme, cvlue);
@@ -99,26 +60,14 @@ public class dynamodbfunctions {
                   Double cvlue= (Double)rw[colcnt];
                   item.withNumber(cnme, cvlue);
               }
-              
-
-              
               colcnt=colcnt+1;
           }
             table.putItem(item);
             arrcnt=arrcnt+1;
         }
         
-        
         //---------------------update db items-----------------
         
-        /*AmazonDynamoDB dynamoDB;
-        dynamoDB = AmazonDynamoDBClientBuilder.standard()
-            .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-            .withRegion("us-east-1")
-            .build();
-        int n=0;
-        Item item = new Item().withPrimaryKey("id", 120);
-        item.withString("name", "Book 120 Title");*/
         
     }
 }
